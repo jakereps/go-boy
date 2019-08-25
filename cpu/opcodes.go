de5535ba3280987ae20910b0cdaecb680a0ceda4
Jorden Kreps <jakereps@users.noreply.github.com>
2019-08-24 17:18:04 -0700

    Finish 8-bit Transfer/IO Instructions - add cycles to comments

--- cpu/opcodes.go
+++ cpu/opcodes.go
@@ -3,7 +3,7 @@ package cpu
 type Opcode byte
 
 var InstructionSet = map[Opcode]Instruction{
-	// LD r,r' (r <- r')
+	// LD r,r' (r <- r') 1
 	Opcode(0x1<<6 | a<<3 | a): LD,
 	Opcode(0x1<<6 | a<<3 | b): LD,
 	Opcode(0x1<<6 | a<<3 | c): LD,
@@ -53,7 +53,7 @@ var InstructionSet = map[Opcode]Instruction{
 	Opcode(0x1<<6 | l<<3 | e): LD,
 	Opcode(0x1<<6 | l<<3 | h): LD,
 	Opcode(0x1<<6 | l<<3 | l): LD,
-	// LD r,n (r <- n)
+	// LD r,n (r <- n) 2
 	Opcode(0x0 | a<<3 | 0x6): LD,
 	Opcode(0x0 | b<<3 | 0x6): LD,
 	Opcode(0x0 | c<<3 | 0x6): LD,
@@ -61,7 +61,7 @@ var InstructionSet = map[Opcode]Instruction{
 	Opcode(0x0 | e<<3 | 0x6): LD,
 	Opcode(0x0 | h<<3 | 0x6): LD,
 	Opcode(0x0 | l<<3 | 0x6): LD,
-	// LD r,(HL) (r <- (HL))
+	// LD r,(HL) (r <- (HL)) 2
 	Opcode(0x1<<6 | a<<3 | 0x6): LD,
 	Opcode(0x1<<6 | b<<3 | 0x6): LD,
 	Opcode(0x1<<6 | c<<3 | 0x6): LD,
@@ -69,7 +69,7 @@ var InstructionSet = map[Opcode]Instruction{
 	Opcode(0x1<<6 | e<<3 | 0x6): LD,
 	Opcode(0x1<<6 | h<<3 | 0x6): LD,
 	Opcode(0x1<<6 | l<<3 | 0x6): LD,
-	// LD (HL),r ((HL) <- r)
+	// LD (HL),r ((HL) <- r) 2
 	Opcode(0x1<<6 | 0x6<<3 | a): LD,
 	Opcode(0x1<<6 | 0x6<<3 | b): LD,
 	Opcode(0x1<<6 | 0x6<<3 | c): LD,
@@ -77,6 +77,34 @@ var InstructionSet = map[Opcode]Instruction{
 	Opcode(0x1<<6 | 0x6<<3 | e): LD,
 	Opcode(0x1<<6 | 0x6<<3 | h): LD,
 	Opcode(0x1<<6 | 0x6<<3 | l): LD,
-	// LD (HL),n ((HL) <- n)
+	// LD (HL),n ((HL) <- n) 3
 	Opcode(0x0 | 0x6<<3 | 0x6): LD,
+	// LD A,(BC) (A <- BC) 2
+	Opcode(0x0 | 0x1<<3 | 0x2): LD,
+	// LD A,(DE) (A <- DE) 2
+	Opcode(0x0 | 0x3<<3 | 0x2): LD,
+	// LD A,(C) (A <- (FF00H+C)) 2
+	Opcode(0x3<<6 | 0x6<<3 | 0x2): LD,
+	// LD (C),A ((FF00H+C) <- A) 2
+	Opcode(0x3<<6 | 0x4<<3 | 0x2): LD,
+	// LD A,(n) (A <- (n)) 3
+	Opcode(0x3<<6 | 0x6<<3 | 0x0): LD,
+	// LD (n),A ((n) <- A) 3
+	Opcode(0x3<<6 | 0x4<<3 | 0x0): LD,
+	// LD A,(nn) (A <- (nn)) 4
+	Opcode(0x3<<6 | 0x7<<3 | 0x2): LD,
+	// LD (nn),A ((nn) <- A) 4
+	Opcode(0x3<<6 | 0x5<<3 | 0x2): LD,
+	// LD A,(HLI) (A <- HL, HL <- HL+1) 2
+	Opcode(0x0 | 0x5<<3 | 0x2): LD,
+	// LD A,(HLD) (A <- HL, HL <- HL-1) 2
+	Opcode(0x0 | 0x7<<3 | 0x2): LD,
+	// LD (BC),A ((BC) <- A) 2
+	Opcode(0x0 | 0x0 | 0x2): LD,
+	// LD (DE),A ((DE) <- A) 2
+	Opcode(0x0 | 0x2<<3 | 0x2): LD,
+	// LD (HLI),A ((HL) <- A, HL <- HL+1) 2
+	Opcode(0x0 | 0x4<<3 | 0x2): LD,
+	// LD (HLD),A ((HL) <- A, HL <- HL-1) 2
+	Opcode(0x0 | 0x6<<3 | 0x2): LD,
 }
